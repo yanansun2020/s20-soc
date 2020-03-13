76737aeaca77eb59b5c1166a6f78bc2fd67e5c36
sunyanan <sunyanan1@xiaomi.com>
2020-03-13 00:25:18 -0700

    remove unused code

--- assignment4/Frontend/app/controllers/HomeController.java
+++ assignment4/Frontend/app/controllers/HomeController.java
@@ -11,9 +11,6 @@ import javax.inject.Inject;
  * Software Service Market Place
  */
 public class HomeController extends Controller {
-    private static final String URL = "http://localhost:9000/";
-    private static final Integer PORT = 9000;
-
     @Inject
     HttpExecutionContext ec;
 
