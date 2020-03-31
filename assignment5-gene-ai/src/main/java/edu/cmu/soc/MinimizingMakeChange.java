c84a806f4023fbc1f9f1257fd51e45f6e0a1f2ce
Yanan Sun <yanansun@andrew.cmu.edu>
2020-03-30 17:46:38 -0700

    assignment5

---
+++ assignment5-gene-ai/src/main/java/edu/cmu/soc/MinimizingMakeChange.java
@@ -0,0 +1,182 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licensing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package edu.cmu.soc;
+
+import org.jgap.Chromosome;
+import org.jgap.Configuration;
+import org.jgap.FitnessFunction;
+import org.jgap.Gene;
+import org.jgap.Genotype;
+import org.jgap.IChromosome;
+import org.jgap.Population;
+import org.jgap.UnsupportedRepresentationException;
+import org.jgap.audit.EvolutionMonitor;
+import org.jgap.data.DataTreeBuilder;
+import org.jgap.data.IDataCreators;
+import org.jgap.impl.DefaultConfiguration;
+import org.jgap.impl.IntegerGene;
+import org.jgap.xml.XMLDocumentBuilder;
+import org.jgap.xml.XMLManager;
+import org.w3c.dom.Document;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class MinimizingMakeChange {
+
+    private static final int MAX_ALLOWED_EVOLUTIONS = 50;
+    public static EvolutionMonitor m_monitor;
+    public static void makeChangeForAmount(boolean a_doMonitor)
+            throws Exception {
+        // Start with a DefaultConfiguration, which comes setup with the
+        // most common settings.
+        // -------------------------------------------------------------
+        Configuration conf = new DefaultConfiguration();
+        // Care that the fittest individual of the current population is
+        // always taken to the next generation.
+        // Consider: With that, the pop. size may exceed its original
+        // size by one sometimes!
+        // -------------------------------------------------------------
+        conf.setPreservFittestIndividual(true);
+        conf.setKeepPopulationSizeConstant(false);
+        // Set the fitness function we want to use, which is our
+        // MinimizingMakeChangeFitnessFunction. We construct it with
+        // the target amount of change passed in to this method.
+        // ---------------------------------------------------------
+        FitnessFunction myFunc =
+                new MinimizingMakeChangeFitnessFunction();
+        conf.setFitnessFunction(myFunc);
+        if (a_doMonitor) {
+            // Turn on monitoring/auditing of evolution progress.
+            // --------------------------------------------------
+            m_monitor = new EvolutionMonitor();
+            conf.setMonitor(m_monitor);
+        }
+        // Now we need to tell the Configuration object how we want our
+        // Chromosomes to be setup. We do that by actually creating a
+        // sample Chromosome and then setting it on the Configuration
+        // object. As mentioned earlier, we want our Chromosomes to each
+        // have four genes, one for each of the coin types. We want the
+        // values (alleles) of those genes to be integers, which represent
+        // how many coins of that type we have. We therefore use the
+        // IntegerGene class to represent each of the genes. That class
+        // also lets us specify a lower and upper bound, which we set
+        // to sensible values for each coin type.
+        // --------------------------------------------------------------
+        Gene[] sampleGenes = new Gene[3];
+        sampleGenes[0] = new IntegerGene(conf, 0, 4); // service cluster1
+        sampleGenes[1] = new IntegerGene(conf, 0, 2); // service cluster 2
+        sampleGenes[2] = new IntegerGene(conf, 0, 7); // service cluster 3
+        IChromosome sampleChromosome = new Chromosome(conf, sampleGenes);
+        conf.setSampleChromosome(sampleChromosome);
+        // Finally, we need to tell the Configuration object how many
+        // Chromosomes we want in our population. The more Chromosomes,
+        // the larger number of potential solutions (which is good for
+        // finding the answer), but the longer it will take to evolve
+        // the population (which could be seen as bad).
+        // ------------------------------------------------------------
+        conf.setPopulationSize(20);
+
+        // Create random initial population of Chromosomes.
+        // Here we try to read in a previous run via XMLManager.readFile(..)
+        // for demonstration purpose only!
+        // -----------------------------------------------------------------
+        Genotype population = Genotype.randomInitialGenotype(conf);;
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < MAX_ALLOWED_EVOLUTIONS; i++) {
+            if (!uniqueChromosomes(population.getPopulation())) {
+                throw new RuntimeException("Invalid state in generation " + i);
+            }
+            if (m_monitor != null) {
+                population.evolve(m_monitor);
+            } else {
+                population.evolve();
+            }
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("Total evolution time: " + (endTime - startTime)
+                + " ms");
+        // Save progress to file. A new run of this example will then be able to
+        // resume where it stopped before! --> this is completely optional.
+        // ---------------------------------------------------------------------
+
+        // Represent Genotype as tree with elements Chromomes and Genes.
+        // -------------------------------------------------------------
+        DataTreeBuilder builder = DataTreeBuilder.getInstance();
+        IDataCreators doc2 = builder.representGenotypeAsDocument(population);
+        // create XML document from generated tree
+        XMLDocumentBuilder docbuilder = new XMLDocumentBuilder();
+        Document xmlDoc = (Document) docbuilder.buildDocument(doc2);
+        XMLManager.writeFile(xmlDoc, new File("JGAPExample26.xml"));
+        // Display the best solution we found.
+        // -----------------------------------
+        IChromosome bestSolutionSoFar = population.getFittestChromosome();
+        double v1 = bestSolutionSoFar.getFitnessValue();
+        System.out.println("The best solution has a fitness value of " +
+                bestSolutionSoFar.getFitnessValue());
+        bestSolutionSoFar.setFitnessValueDirectly(-1);
+        System.out.println("It contains the following services: ");
+        int serviceIndex0 =  MinimizingMakeChangeFitnessFunction.getIndexOfService(
+                        bestSolutionSoFar, 0);
+        String serviceName0 = MinimizingMakeChangeFitnessFunction.serviceFeatures.get(0).
+                get(serviceIndex0).getServiceName();
+        int serviceIndex1= MinimizingMakeChangeFitnessFunction.getIndexOfService(
+                        bestSolutionSoFar, 1);
+        String serviceName1 = MinimizingMakeChangeFitnessFunction.serviceFeatures.get(1)
+                .get(serviceIndex1).getServiceName();
+        int serviceIndex2 = MinimizingMakeChangeFitnessFunction.getIndexOfService(
+                bestSolutionSoFar, 2);
+        String serviceName2 = MinimizingMakeChangeFitnessFunction.serviceFeatures.get(2)
+                .get(serviceIndex2).getServiceName();
+
+        System.out.println( "Service index " + serviceIndex0 + " which is " + serviceName0+ " in SC1");
+        System.out.println( "Service index " + serviceIndex1 + " which is " + serviceName1+ " in SC2");
+        System.out.println( "Service index " + serviceIndex2 + " which is " + serviceName2+ " in SC3");
+    }
+
+    /**
+     * Main method. A single command-line argument is expected, which is the
+     * amount of change to create (in other words, 75 would be equal to 75
+     * cents).
+     *
+     * @param args amount of change in cents to create
+     * @throws Exception
+     * @author Neil Rotstan
+     * @author Klaus Meffert
+     * @since 1.0
+     */
+    public static void main(String[] args) {
+        try {
+            makeChangeForAmount(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @param a_pop the population to verify
+     * @return true if all chromosomes in the populationa are unique
+     * @author Klaus Meffert
+     * @since 3.3.1
+     */
+    public static boolean uniqueChromosomes(Population a_pop) {
+        // Check that all chromosomes are unique
+        for (int i = 0; i < a_pop.size() - 1; i++) {
+            IChromosome c = a_pop.getChromosome(i);
+            for (int j = i + 1; j < a_pop.size(); j++) {
+                IChromosome c2 = a_pop.getChromosome(j);
+                if (c == c2) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
---
+++ assignment5-gene-ai/src/main/java/edu/cmu/soc/ReadServiceFeatures.java
@@ -0,0 +1,180 @@
+package edu.cmu.soc;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+public class ReadServiceFeatures {
+    private static final String TIME_RANGE = "time_range";
+    private static final String TIME_MIN = "time_min";
+    private static final String COST_RANGE = "cost_range";
+    private static final String COST_MIN = "cost_min";
+    public static void main(String[] args) {
+        Map<Integer, List<ServiceFeature>> features = getPercentageMap("sc.xlsx");
+        System.out.println(1);
+    }
+    static Map<Integer, List<ServiceFeature>> getFeaturesMap(){
+        Map<Integer, List<ServiceFeature>> features = getPercentageMap("sc.xlsx");
+        return features;
+    }
+
+    private static Map<Integer, List<ServiceFeature>> getPercentageMap(String fileName){
+        Map<Integer, List<ServiceFeature>> featuresMap = readFromConfig(fileName);
+        Map<String, Double> timeMap = getRangeAndMinValueForTime(featuresMap);
+        Map<String, Double> costMap = getRangeAndMinValueForCost(featuresMap);
+        Double minTime = timeMap.get(TIME_MIN);
+        Double timeRange = timeMap.get(TIME_RANGE);
+        Double minCost = costMap.get(COST_MIN);
+        Double costRange = costMap.get(COST_RANGE);
+        for(Map.Entry<Integer, List<ServiceFeature>> entry : featuresMap.entrySet()){
+            List<ServiceFeature> features = entry.getValue();
+            for(ServiceFeature feature : features){
+                feature.setAvailability(feature.getAvailabilityI()/100);
+                feature.setReliability(feature.getReliabilityI()/100);
+                feature.setTime(normalize(feature.getTimeI(), minTime, timeRange));
+                feature.setCost(normalize(feature.getCostI(), minCost, costRange));
+            }
+        }
+        return featuresMap;
+    }
+
+    private static Map<String, Double> getRangeAndMinValueForTime(Map<Integer, List<ServiceFeature>> featuresMap){
+        List<ServiceFeature> features = getAllFeatures(featuresMap);
+        Double timeRange = getTimeRange(features);
+        Double minTime = features.get(features.size()-1).getTimeI();
+        Map<String, Double> timeResult = new HashMap<String, Double>();
+        timeResult.put(TIME_RANGE, timeRange);
+        timeResult.put(TIME_MIN, minTime);
+        return timeResult;
+    }
+
+    private static Map<String, Double> getRangeAndMinValueForCost(Map<Integer, List<ServiceFeature>> featuresMap){
+        List<ServiceFeature> features = getAllFeatures(featuresMap);
+        Double costRange = getCostRange(features);
+        Double mincost = features.get(features.size()-1).getCostI();
+        Map<String, Double> costResult = new HashMap<String, Double>();
+        costResult.put(COST_RANGE, costRange);
+        costResult.put(COST_MIN, mincost);
+        return costResult;
+    }
+
+
+    private static List<ServiceFeature> getAllFeatures( Map<Integer, List<ServiceFeature>> featuresMap ){
+        List<ServiceFeature> allFeatures = new ArrayList<ServiceFeature>();
+        for(Map.Entry<Integer, List<ServiceFeature>> entry : featuresMap.entrySet()){
+            allFeatures.addAll(entry.getValue());
+        }
+        return allFeatures;
+    }
+
+    private static Double normalize(Double timeI, Double minTime, Double range) {
+        return (timeI-minTime)/range;
+    }
+
+    private static Double getTimeRange(List<ServiceFeature> features) {
+        Collections.sort(features, new Comparator<ServiceFeature>() {
+            public int compare(ServiceFeature o1, ServiceFeature o2) {
+                if(o1.getTimeI().equals(o2.getTimeI())){
+                    return 0;
+                }
+                return o1.getTimeI() < o2.getTimeI() ? 1 : -1;
+            }
+        });
+        return Math.abs(features.get(0).getTimeI() - features.get(features.size()-1).getTimeI());
+    }
+
+    private static Double getCostRange(List<ServiceFeature> features) {
+        Collections.sort(features, new Comparator<ServiceFeature>() {
+            public int compare(ServiceFeature o1, ServiceFeature o2) {
+                if(o1.getCostI().equals(o2.getCostI())){
+                    return 0;
+                }
+                return o1.getCostI() < o2.getCostI() ? 1 : -1;
+            }
+        });
+        return Math.abs(features.get(0).getCostI() - features.get(features.size()-1).getCostI());
+    }
+
+
+    static Map<Integer, List<ServiceFeature>> readFromConfig(String fileName){
+        Map<Integer, List<ServiceFeature>> featureMap = new HashMap<Integer, List<ServiceFeature>>();
+        try {
+            //Create Workbook instance holding reference to .xlsx file
+            XSSFWorkbook workbook = new XSSFWorkbook(ClassLoader.getSystemResourceAsStream(fileName));
+
+            //Get first/desired sheet from the workbook
+            XSSFSheet sheet = workbook.getSheetAt(0);
+
+            //Iterate through each rows one by one
+            Iterator<Row> rowIterator = sheet.iterator();
+            Row headerRow= rowIterator.next();
+            while (rowIterator.hasNext()) {
+                Row row = rowIterator.next();
+                //For each row, iterate through all the columns
+                Iterator<Cell> cellIterator = row.cellIterator();
+                int n = 0;
+                Integer serviceCluster = -1;
+                List<ServiceFeature> features;
+                ServiceFeature feature = new ServiceFeature();
+                while (cellIterator.hasNext()) {
+                    Cell cell = cellIterator.next();
+                    //Check the cell type and format accordingly
+                    switch (n){
+                        case 0:
+                            String serviceClusterStr = cell.getStringCellValue();
+                            if(serviceClusterStr.equals("SC1")){
+                                serviceCluster = 0;
+                            }
+                            if(serviceClusterStr.equals("SC2")){
+                                serviceCluster = 1;
+                            }
+                            if(serviceClusterStr.equals("SC3")){
+                                serviceCluster = 2;
+                            }
+                            break;
+                        case 1:
+                            String serviceName = cell.getStringCellValue();
+                            feature.setServiceName(serviceName);
+                            break;
+                        case 2:
+                            Double cost = cell.getNumericCellValue();
+                            feature.setCostI(cost);
+                            break;
+                        case 3:
+                            Double reliability = cell.getNumericCellValue();
+                            feature.setReliabilityI(reliability);
+                            break;
+                        case 4:
+                            Double time = cell.getNumericCellValue();
+                            feature.setTimeI(time);
+                            break;
+                        case 5:
+                            Double available = cell.getNumericCellValue();
+                            feature.setAvailabilityI(available);
+                            break;
+                    }
+                    n++;
+                }
+                if(featureMap.containsKey(serviceCluster)){
+                    features = featureMap.get(serviceCluster);
+                } else {
+                    features = new ArrayList<ServiceFeature>();
+                    featureMap.put(serviceCluster, features);
+                }
+                features.add(feature);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return featureMap;
+    }
+}
